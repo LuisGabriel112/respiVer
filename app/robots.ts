e0e9e2272b0910fddfccc423cc99214e4b93157c
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2026-09-15 19:32:12 -0600

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,12 @@
 import { MetadataRoute } from 'next'
-
+ 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://respi-ver.vercel.app/sitemap.xml',
+    sitemap: 'https://respiver.com.mx/sitemap.xml',
   }
 }
+ 
